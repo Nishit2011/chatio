99c20854d3e363fe749adfef8c8a9e708e5525d6
Nishit Ranjan (BAS) <NI324452@wipro.com>
2016-04-06 20:53:58 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,57 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    usernames=[];
+    server.listen(process.env.PORT ||  3000);
+    console.log('Server Running');
+
+    //directing the root ur to index.html
+    app.get('/',function(req,res){
+
+    	res.sendFile(__dirname +'/index.html' );
+    });
+
+    //connecting to socket
+    io.sockets.on('connection',function(socket){
+
+    	console.log('Socket Connected...');
+
+    	socket.on('new user',function(data,callback){
+    		if(usernames.indexOf(data) != -1){
+
+    			callback(false);
+    		}else{
+    			callback(true);
+    			socket.username = data;
+    			usernames.push(socket.username);
+    			updateUsernames(); 
+    		}
+
+    	});
+
+    	function updateUsernames(){
+
+    		io.sockets.emit('usernames',usernames);
+    	}
+
+    	//Send Message will send message from the server to the client
+    	socket.on('send message',function(data){
+
+    		io.sockets.emit('new message',{msg:data,user:socket.username});
+    	});
+
+    	//Disconnect
+
+    	socket.on('disconnect',function(data){
+
+    		if(!socket.username){
+    			return;
+    		}
+
+    		//we want to take username out of array
+
+    		usernames.splice(usernames.indexOf(socket.username),1);
+    		updateUsernames();
+    	})
+    });
